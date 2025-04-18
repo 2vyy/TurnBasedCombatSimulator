3ecb1f5ade61f8c29f54bb6c78b77b2db0114132
ivy <thatjaredbrown@gmail.com>
2025-04-18 18:55:01 -0500

    Add unit test for combat_system and fix the argument order for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
 ﻿#include "combat_system.h"
-#include "character.h"
-#include "team.h"
 #include <iostream>
 
 int main() {
-    Character goblin1{ "Red Goblin", 17, 100, 76 };
-    Character goblin2{ "Blue Goblin", 26, 120, 50 };
-    Character goblin3{ "Green Goblin", 21, 105, 60 };
+    Character goblin1{ "Red Goblin", 100, 17, 75 };
+    Character goblin2{ "Blue Goblin", 120, 26, 50 };
+    Character goblin3{ "Green Goblin", 105, 21, 60 };
     Team goblins{ "Goblin" };
     goblins.add_member(goblin1);
     goblins.add_member(goblin2);
     goblins.add_member(goblin3);
     
-    Character knight2{ "Iron Knight", 22, 110, 65 };
-    Character knight1{ "Silver Knight", 28, 125, 55 };
-    Character knight3{ "Bronze Knight", 18, 95, 80 };
+    Character knight2{ "Iron Knight", 110, 22, 65 };
+    Character knight1{ "Silver Knight", 125, 28, 55 };
+    Character knight3{ "Bronze Knight", 95, 18, 80 };
     Team knights{ "Knights" };
     knights.add_member(knight1);
     knights.add_member(knight2);
--- tests/test_combat_system.cpp
+++ tests/test_combat_system.cpp
@@ -1,1 +1,17 @@
+#include "combat_system.h"
+#include <catch2/catch_test_macros.hpp>
 
+TEST_CASE("Action Value Test") {
+	Character fast{ "Test Character", 100, 50, 50 };
+	Character slow{ "Test Character", 100, 50, 25 };
+
+	Team fast_team{ "Fast Team" };
+	fast_team.add_member(fast);
+	Team slow_team{ "Slow Team" };
+	fast_team.add_member(slow);
+
+	Combat_System combat_system{ fast_team, slow_team };
+	combat_system.start_combat();
+
+	REQUIRE(slow_team.is_defeated());
+}
